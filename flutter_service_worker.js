2452bb5a18ded3be8f7fc49c758d6b5f731f619b
Hermann <63232411+hermannkabi@users.noreply.github.com>
2021-03-09 10:40:22 +0200

    Added StayLoggedIn to Auth
    StayLoggedIn is now available for QuantoAuth. If you choose Stay Logged In, that will still be available to the app, but now it automatically saves your account to QuantoAuth so that the next time you will just have to click your account.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8de9862da712841485f22bc31d54cce1",
-"index.html": "32a8d3dd631f274f9c3fca274cc79688",
-"/": "32a8d3dd631f274f9c3fca274cc79688",
-"main.dart.js": "4c6159e23a581e7810919703f22d5e0b",
+"index.html": "cff3d9d1869c363d772b62e78102cec6",
+"/": "cff3d9d1869c363d772b62e78102cec6",
+"main.dart.js": "011df04c2408ed684e143a121f13123d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "310f612a4ac4c853b4821ac17887c358",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "e1d83179a5f5855575b2d7ec0ec90e01",
+"assets/NOTICES": "656dcc0c2e0dfb8c8c54ac6c4429f988",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
